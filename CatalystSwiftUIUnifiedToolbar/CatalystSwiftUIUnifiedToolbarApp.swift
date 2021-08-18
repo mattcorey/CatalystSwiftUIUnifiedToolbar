27fead68f252eef198a3da997e848b88dba98ae4
Matt Corey <matt.corey@gmail.com>
2021-08-17 21:14:10 -0400

    Initial Commit

---
+++ CatalystSwiftUIUnifiedToolbar/CatalystSwiftUIUnifiedToolbarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatalystSwiftUIUnifiedToolbarApp.swift
+//  CatalystSwiftUIUnifiedToolbar
+//
+//  Created by Matthew Corey on 8/17/21.
+//
+
+import SwiftUI
+
+@main
+struct CatalystSwiftUIUnifiedToolbarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
